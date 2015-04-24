3d2a841a8f3a9c3c11153c18d0a1538e8aec6865
nikjdwhite <ndjeannio@gmail.com>
2015-04-24 17:18:58 -0500

    JS moves
    Moved scripts around.  Slider markup is now showing up in source but not
    displaying images correctly.

--- js/loadcards.js
+++ js/loadcards.js
@@ -20,7 +20,21 @@ $(document).ready(function() {
 		target.innerHTML = html;
     });
 	
-	
+	$(function() {
+      var $slides = $('#slides');
+
+      Hammer($slides[0]).on("swipeleft", function(e) {
+        $slides.data('superslides').animate('next');
+      });
+
+      Hammer($slides[0]).on("swiperight", function(e) {
+        $slides.data('superslides').animate('prev');
+      });
+
+      $slides.superslides({
+        hashchange: true
+      });
+    });
 	
 	
 
